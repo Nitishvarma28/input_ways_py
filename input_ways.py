8b93076de910c360985f895e7287a86dccaab3a0
nitishvarma28 <102401628+Nitishvarma28@users.noreply.github.com>
2022-03-31 15:46:37 +0530

    Add files via upload

---
+++ input_ways.py
@@ -0,0 +1,53 @@
+# a=input('NITISH VARMA:')
+# print("NITISH VARMA",a)
+# print("NITISH VARMA",a,sep="-")
+# print("NITISH VARMA",int(a))  
+# print("NITISH VARMA",float(a))
+# print("NITISH VARMA"+' '+str(a))
+# print("NITISH VARMA"+' '+a)
+
+# a=int(input('9666464748:'))
+# print("9666464748",a)
+# print("9666464748",a,sep=" ")
+# print("9666464748",int(a))
+# print("9666464748",float(a))
+# print("9666464748"+' '+str(a))
+
+# a=float(input('Enter your BMI:'))
+# print("Your BMI is",a)
+# print("Your BMI is",a,sep=" ")
+# print("Your BMI is",int(a))
+# print("Your BMI is",float(a))
+# print("Your BMI is"+' '+str(a))
+
+# name=input("NITISH VARMA:")
+# age=int(input("20:"))
+# per=float(input("Enter your Graduation Percentage:"))
+
+# print(name,age,per)
+
+# print(age,name,per)
+
+# print(name,age,name)
+
+# name=input("Enter Your Name:")
+# age=int(input("Enter Your Age:"))
+# per=float(input("Enter your Graduation Percentage:"))
+
+# print("Your Name is",name,"and age is",age,\
+# "Graduation percentage is",per)
+
+# print("Your Name is"+' '+name,"and age is",age, \
+# "Graduation percentage is",str(per)+'%')
+
+# print("Your Name is %s and age is %d. \
+# Graduation percentage is %.2f"%(name,age,per))
+
+# print("Your Name is {} and age is {}. \
+# Graduation percentage is {}".format(name,age,per))
+
+# print("Your Name is {1} and age is {0}. \
+#  Graduation percentage is {2}".format(age,name,per))
+
+# print(f"Your Name is {name} and age is {age}.\
+# Graduation percentage is {per}")
